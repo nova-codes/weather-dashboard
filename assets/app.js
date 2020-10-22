7cb22837a7e6d7dccaab8184cf0c4b46cf0bb60f
Hannah Elliott <hgelliott90@gmail.com>
2020-10-22 02:34:35 -0400

    Almost done with the script.

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 // global variables
 let cityName;
+let stateCode;
 let searchHistory = [];
 
 // current date in the weather card
@@ -54,7 +55,7 @@ function listCities() {
         cityButton.addClass('pill');
         cityButton.attr('data-name', searchHistory[i]);
         cityButton.text(searchHistory[i]);
-        $('.tophistory').prepend(cityButton); 
+        $('.tophistory').append(cityButton); 
     }
 }
 
@@ -71,7 +72,7 @@ function showButtons() {
 
 // toss the cities into local storage so they can persist
 function saveSearch() {
-    localStorage.setItem('currentCity', JSON.stringify(cityName));
+    localStorage.setItem('currentCity', JSON.stringify(cityName + ', ' + stateCode));
     localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
 }
 
@@ -88,7 +89,7 @@ function previousCity() {
 
 async function showWeather() {
     // openweathermap api call
-    var openWeatherMap = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=0db11239e44307838ff3571b683ebdc1";
+    var openWeatherMap = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ', '+ stateCode + "&units=imperial&appid=0db11239e44307838ff3571b683ebdc1";
 
     await $.ajax({
         url: openWeatherMap,
@@ -96,15 +97,47 @@ async function showWeather() {
     }).then(function(response) {
         console.log(response);
 
-        $('.city-name').text(response.name); 
+        $('.city-name').text(response.city.name); 
 
         // pull data for today's weather
         // icon
         var prefix = 'wi wi-owm-';
-        var code = response.weather[0].id;
+        var code = response.list[0].weather[0].id;
         var tag = $('<i>');
         var icon = tag.addClass(prefix + code + ' big-bg');
 
         $('.weather-icon').html(icon); 
+        
+        // other weather data
+        $('#current-weather').text(response.list[0].weather[0].main); 
+        $('.degree').text(response.list[0].main.temp);
+        $('.humidity').text(response.list[0].main.humidity);
+        $('.wind-speed').text(response.list[0].wind.speed); 
+
+        // uv index
+        var latitude = response.city.coord.lat;
+        var longitude = response.city.coord.lon;
+        var uvurl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=0db11239e44307838ff3571b683ebdc1";
+
+        $.ajax({
+            url: uvurl,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            $("#uv-text").text("UV Index: ")
+            $("#current-uv").text(response.value);
+            $("#current-uv").removeClass("uv-low uv-moderate uv-high uv-vhigh uv-extreme");
+            if (response.value <= 2.99) {
+                $("#current-uv").addClass("uv-low");
+            } else if (response.value >= 3 && response.value <= 5.99) {
+                $("#current-uv").addClass("uv-moderate");
+            } else if (response.value >= 6 && response.value <= 7.99) {
+                $("#current-uv").addClass("uv-high");
+            } else if (response.value >= 8 && response.value <= 10) {
+                $("#current-uv").addClass("uv-vhigh");
+            } else {
+                $("#current-uv").addClass("uv-extreme");
+            }
+        })
     })
 }
